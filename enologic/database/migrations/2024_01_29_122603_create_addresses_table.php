ff29f231daf46e81a171276cc5977b906bdee5f7
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2024-01-29 13:29:14 +0100

    Migrations Addresses

---
+++ enologic/database/migrations/2024_01_29_122603_create_addresses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('street');
+            $table->string('city');
+            $table->string('state');
+            $table->string('zipcode');
+            $table->timestamps();
+            $table->foreignId('users_id')->references('id')->on('users');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
