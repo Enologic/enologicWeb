7742ec150b3973553862139b414f864b410a2ac3
Fernando Rojas <rojasdsa@alumnos.ilernasevilla.com>
2024-01-30 09:27:46 +0100

    mod> VITE, add > bootstrap, layouts, head-routes

---
+++ enologic/resources/views/layouts/general.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.template')
+
+@section('general')
+    {{-- HEADER --}}
+    <nav>
+    </nav>
+
+    {{-- Vista principal --}}
+    {{-- Aquí irán todos los yields de las vistas que tengamos. --}}
+
+    {{-- FOOTER --}}
+    <footer>
+    </footer>
+@endsection
--- enologic/vite.config.js
+++ enologic/vite.config.js
@@ -4,7 +4,13 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.scss', //Bootstrap
+                'resources/css/app.css', // Debe estar vacio para que lo compile SASS
+                'resources/css/custom.scss', 
+                'resources/css/custom.css', 
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
