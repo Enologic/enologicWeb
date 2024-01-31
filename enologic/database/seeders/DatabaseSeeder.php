d029c042acc9ee2245d208d777860430fbb58382
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2024-01-31 18:03:51 +0100

    Seeder Basico Product

--- enologic/database/seeders/DatabaseSeeder.php
+++ enologic/database/seeders/DatabaseSeeder.php
@@ -1,12 +1,12 @@
 <?php
 
 namespace Database\Seeders;
-
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+
     /**
      * Seed the application's database.
      */
@@ -18,5 +18,6 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+    $this->call(ProductSeeder::class);
     }
 }
