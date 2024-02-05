c1a09e53c2202efdea8d0c9f98963fed08f4cbf4
Fernando Rojas <rojasdsa@alumnos.ilernasevilla.com>
2024-02-05 23:43:35 +0100

    ADD => confirm order

--- enologic/resources/views/layouts/cart.blade.php
+++ enologic/resources/views/layouts/cart.blade.php
@@ -56,8 +56,9 @@
                                 <div class="modal-body mt-2 text-center">
                                     <p class="m-0 fw-medium">Do you want to remove from cart this product?
                                     </p>
-                                    <p class="fw-bolder mt-3">{{ $product->product_name }}</p> 
-                                    <p class="mt-3"><span class="fw-medium">x{{ $product->pivot->quantity }}</span> Units</p> 
+                                    <p class="fw-bolder mt-3">{{ $product->product_name }}</p>
+                                    <p class="mt-3"><span class="fw-medium">x{{ $product->pivot->quantity }}</span> Units
+                                    </p>
                                 </div>
                                 <div class="bg-dark modal-footer justify-content-center">
                                     <form action="{{ route('delete.producto', $product->id) }}" method="POST">
@@ -65,8 +66,7 @@
                                         @method('DELETE')
                                         <button type="submit" class="btn btn-danger">Yes, delete</button>
                                     </form>
-                                    <button type="button" class="btn btn-secondary"
-                                        data-bs-dismiss="modal">Back</button>
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Back</button>
                                 </div>
                             </div>
                         </div>
@@ -76,11 +76,65 @@
         </table>
 
         <div class="text-end mt-3">
-            <h5>Total amount:
-                {{ $products->sum(function ($product) {return $product->price * $product->pivot->quantity;}) }} €</h5>
+            <h5>Total:
+                {{ $products->sum(function ($product) {return $product->price * $product->pivot->quantity;}) }} €
+            </h5>
+            {{-- Botón para añadir un producto --}}
+            <div class="mt-3">
+                <div class="d-inline-block">
+                    <a href="#" class="btn btn-success mx-1" data-bs-toggle="modal" data-bs-target="#sendOrderModal">
+                        Send order
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
+    {{-- Modal para agregar un producto --}}
+    <div class="modal fade" id="sendOrderModal" tabindex="-1" aria-labelledby="addProductModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header bg-dark text-white">
+                    <h5 class="modal-title" id="addProductModalLabel">Confirm order</h5>
+                    <button type="button" class="btn-close bg-danger rounded-5" data-bs-dismiss="modal"
+                        aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{ route('guardar.producto') }}" method="POST">
+                        @csrf
 
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Name</th>
+                                    <th scope="col" class="text-center">Price</th>
+                                    <th scope="col" class="text-center">Quantity</th>
+                                    <th scope="col" class="text-center">Total</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($products as $product)
+                                    <tr class="align-middle">
+                                        <td>{{ $product->product_name }}</td>
+                                        <td class="text-center">{{ $product->price }} €</td>
+                                        <td class="text-center">{{ $product->pivot->quantity }}</td>
+                                        <td class="text-center">{{ $product->price * $product->pivot->quantity }} €</td>
+                                    </tr>
+                            </tbody>
+                            @endforeach
+                        </table>
+                        <h5 class="text-end">Total:
+                            {{ $products->sum(function ($product) {return $product->price * $product->pivot->quantity;}) }} €
+                        </h5>
+
+                </div>
+                <div class="modal-footer justify-content-center bg-dark">
+                    <button type="submit" class="btn btn-success">Confirm order</button>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
     {{-- FOOTER --}}
     <footer>
