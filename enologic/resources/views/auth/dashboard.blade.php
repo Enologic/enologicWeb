4e7c203e2e568c13a66024a9d71b48ed92d77f72
Fernando Rojas <rojasdsa@alumnos.ilernasevilla.com>
2024-02-05 17:57:46 +0100

    ADD => acceso a vistas desde login

--- enologic/resources/views/auth/dashboard.blade.php
+++ enologic/resources/views/auth/dashboard.blade.php
@@ -1,24 +1,48 @@
 @extends('auth.template')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">
-                    Dashboard
-                </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        Dashboard
+                    </div>
 
-                <div class="card-body">
-                    @if(session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{session('status')}}
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        You are logged in!
+                    </div>
+                </div>
+                <div class="card mt-5 border-dark">
+                    <div class="card-header bg-warning fw-medium border-dark">
+                        Vistas
+                    </div>
+
+                    <div class="card-body d-flex justify-content-center">
+
+                        <div class="col-6 text-center">
+                            {{-- USER --}}
+                            <a class="btn btn-info border-dark mb-3" href="{{ route('show') }}">
+                                {{ __('Blade show') }}
+                            </a>
+                            <p>Vista para el user</p>
                         </div>
-                    @endif
-                    You are logged in!
+
+                        <div class="col-6 text-center">
+                            {{-- ADMIN --}}
+                            <a class="btn btn-info border-dark mb-3" href="{{ route('add') }}">
+                                {{ __('Blade add') }}
+                            </a>
+                            <p>Vista para el user</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
