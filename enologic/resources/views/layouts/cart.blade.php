47df05fb52b5373c2286336c534717c031424175
Fernando Rojas <rojasdsa@alumnos.ilernasevilla.com>
2024-02-05 22:52:21 +0100

    ADD/MOD => buttons, routes, style

--- enologic/resources/views/layouts/cart.blade.php
+++ enologic/resources/views/layouts/cart.blade.php
@@ -7,68 +7,81 @@
 
     {{-- Vista principal --}}
     {{-- Aquí irán todos los yields de las vistas que tengamos. --}}
-   
-        <div class="container mt-4">
-            <h2>Carrito de Compras</h2>
 
-            <table class="table mt-3">
-    <thead>
-        <tr>
-            <th scope="col">Producto</th>
-            <th scope="col">Precio</th>
-            <th scope="col">Cantidad</th>
-            <th scope="col">Total</th>
-            <th></th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($products as $product)
-            <tr>
-                <td>{{ $product->product_name }}</td>
-                <td>{{ $product->price }} €</td>
-                <td>{{ $product->pivot->quantity }}</td>
-                <td>{{ $product->price * $product->pivot->quantity }} €</td>
-                <td><a href="#deleteModal{{ $product->id }}" id="img-style-size" class="btn btn-danger mx-1"
-                        data-bs-toggle="modal" data-bs-target="#deleteModal{{ $product->id }}">
-                        <i class="fa-solid fa-trash"></i>
-                    </a></td>
-            </tr>
-            {{-- Modal para eliminar un producto del carrito --}}
-<div class="modal fade" id="deleteModal{{ $product->id }}" tabindex="-1" aria-labelledby="deleteModalLabel"
-    aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="deleteModalLabel">Eliminar Producto del Carrito</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    <div class="container mt-5">
+        <div class="container d-flex">
+
+            <h1 class="col-9 mb-0">Shopping cart - USER</h1>
+
+            <div class="col-3 d-flex justify-content-end align-items-center">
+                {{-- Boton para volver a DASHBOARD --}}
+                <a class="btn btn-dark" href="{{ 'show' }}">
+                    {{ __('Back') }}
+                </a>
             </div>
-            <div class="modal-body">
-                <p class="m-0 fw-medium">¿Estás seguro de que quieres eliminar
-                    <span class="fw-bolder">{{ $product->product_name }}</span> del carrito?
-                </p>
-            </div>
-            <div class="modal-footer justify-content-center">
-                <form action="{{ route('delete.producto', $product->id) }}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-danger">Sí, eliminar</button>
-                </form>
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-            </div>
+
+        </div>
+        <table class="table mt-3">
+            <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col" class="text-center">Price</th>
+                    <th scope="col" class="text-center">Quantity</th>
+                    <th scope="col" class="text-center">Total</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($products as $product)
+                    <tr class="align-middle">
+                        <td>{{ $product->product_name }}</td>
+                        <td class="text-center">{{ $product->price }} €</td>
+                        <td class="text-center">{{ $product->pivot->quantity }}</td>
+                        <td class="text-center">{{ $product->price * $product->pivot->quantity }} €</td>
+                        <td><a href="#deleteModal{{ $product->id }}" id="img-style-size" class="btn btn-danger mx-1"
+                                data-bs-toggle="modal" data-bs-target="#deleteModal{{ $product->id }}">
+                                <i class="fa-solid fa-trash"></i>
+                            </a></td>
+                    </tr>
+                    {{-- Modal para eliminar un producto del carrito --}}
+                    <div class="modal fade" id="deleteModal{{ $product->id }}" tabindex="-1"
+                        aria-labelledby="deleteModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header bg-dark text-white">
+                                    <h5 class="modal-title" id="deleteModalLabel">Remove from cart</h5>
+                                    <button type="button" class="btn-close bg-danger rounded-5" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body mt-2 text-center">
+                                    <p class="m-0 fw-medium">Do you want to remove from cart this product?
+                                    </p>
+                                    <p class="fw-bolder mt-3">{{ $product->product_name }}</p> 
+                                    <p class="mt-3"><span class="fw-medium">x{{ $product->pivot->quantity }}</span> Units</p> 
+                                </div>
+                                <div class="bg-dark modal-footer justify-content-center">
+                                    <form action="{{ route('delete.producto', $product->id) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger">Yes, delete</button>
+                                    </form>
+                                    <button type="button" class="btn btn-secondary"
+                                        data-bs-dismiss="modal">Back</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </tbody>
+        </table>
+
+        <div class="text-end mt-3">
+            <h5>Total amount:
+                {{ $products->sum(function ($product) {return $product->price * $product->pivot->quantity;}) }} €</h5>
         </div>
     </div>
-</div>
 
-        @endforeach
-    </tbody>
-</table>
 
-<div class="text-end mt-3">
-    <h5>Precio Total: {{ $products->sum(function ($product) { return $product->price * $product->pivot->quantity; }) }} €</h5>
-</div>
-        </div>
- 
-    
     {{-- FOOTER --}}
     <footer>
     </footer>
--- enologic/resources/views/layouts/show.blade.php
+++ enologic/resources/views/layouts/show.blade.php
@@ -2,19 +2,23 @@
 
 @section('general')
     <div class="container mt-5">
-        
-       <div class="container d-flex">
-        
-        <h1 class="col-9">Tabla Productos - USER</h1>
 
-        <div class="col-3 text-end"> 
-         {{-- Boton para volver a DASHBOARD --}}
-         <a class="btn btn-dark mb-3" href="{{ ('home') }}">
-             {{ __('Volver') }}
-         </a>
+        <div class="container d-flex">
+
+            <h1 class="col-9">Products - USER</h1>
+
+            <div class="col-3 text-end">
+                {{-- Boton para volver a DASHBOARD --}}
+                <a class="btn btn-dark mb-3" href="{{ 'home' }}">
+                    {{ __('Back') }}
+                </a>
+                {{-- Boton para ir a CART --}}
+                <a class="btn btn-secondary mb-3" href="{{ 'cart' }}">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                </a>
+            </div>
+
         </div>
-
-       </div>
         <table class="table">
             <thead>
                 <tr class="">
@@ -47,39 +51,45 @@
                                 </a>
 
                                 {{-- Modal para añadir un producto --}}
-<div class="modal fade" id="addProductModal{{ $product->id }}" tabindex="-1" aria-labelledby="addProductModalLabel"
-    aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <form action="{{ route('cart.add', ['productId' => $product->id]) }}" method="post">
-                @csrf
-                <div class="modal-header">
-                    <h5 class="modal-title" id="addProductModalLabel">{{ $product->product_name }}</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body d-flex align-items-center">
-                    <div class="col-6 m-0 fw-medium">
-                        Selecciona una cantidad:
-                    </div>
-                    <div class="col-6">
-                        <select class="form-select" id="quantitySelect{{ $product->id }}" name="quantity">
-                            @for ($i = 1; $i <= 10; $i++)
-                                <option value="{{ $i }}">{{ $i }}</option>
-                            @endfor
-                        </select>
-                    </div>
-                </div>
-                <div class="container pb-3 px-3 ">
-                    {{ $product->description }}
-                </div>
-                <div class="modal-footer justify-content-center">
-                    <button type="submit" class="px-4 btn btn-success">Add</button>
-                    <button type="button" class="px-4 btn btn-secondary" data-bs-dismiss="modal">Back</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
+                                <div class="modal fade" id="addProductModal{{ $product->id }}" tabindex="-1"
+                                    aria-labelledby="addProductModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <form action="{{ route('cart.add', ['productId' => $product->id]) }}"
+                                                method="post">
+                                                @csrf
+                                                <div class="modal-header bg-dark text-white">
+                                                    <h5 class="modal-title" id="addProductModalLabel">
+                                                        {{ $product->product_name }}</h5>
+                                                    <button type="button" class="btn-close bg-danger rounded-5"
+                                                        data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body d-flex align-items-center justify-content-center">
+                                                    <div class="col-4 m-0 fw-medium text-center">
+                                                        Choose quantity:
+                                                    </div>
+                                                    <div class="col-4">
+                                                        <select class="form-select" id="quantitySelect{{ $product->id }}"
+                                                            name="quantity">
+                                                            @for ($i = 1; $i <= 10; $i++)
+                                                                <option value="{{ $i }}">{{ $i }}
+                                                                </option>
+                                                            @endfor
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                                <div class="container pb-3 px-3 text-center">
+                                                    {{ $product->description }}
+                                                </div>
+                                                <div class="modal-footer justify-content-center bg-dark">
+                                                    <button type="submit" class="px-4 btn btn-success">Add</button>
+                                                    <button type="button" class="px-4 btn btn-secondary"
+                                                        data-bs-dismiss="modal">Back</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
 
                         </td>
 
