e8949238d029373065c4325bf2fec1c96a499e19
Fernando Rojas <rojasdsa@alumnos.ilernasevilla.com>
2024-02-06 13:50:05 +0100

    ADD => alerts 'auth'

--- enologic/resources/views/auth/forgot-password.blade.php
+++ enologic/resources/views/auth/forgot-password.blade.php
@@ -1,46 +1,53 @@
 @extends('auth.template')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        @if(session('status'))
-        <div class="alert alert-success" role="alert">
-            {{session('status')}}
-        </div>
-        @endif
-        <div class="col-md-8">
-            <div class="card border-dark">
-                <div class="card-header bg-warning fw-medium border-dark">{{ __('Reset password') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            @if (session('status'))
+                <div class="container d-flex justify-content-center">
+                    <div class="col-4 alert alert-info alert-dismissible fade show text-center" role="alert">
+                        <strong>Reset link send. Check your SPAM folder</strong> 
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                </div>
+            @endif
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.request') }}">
-                        @csrf
 
-                        <div class="form-group row mb-3 justify-content-center d-flex">
-                            <label for="email" class="col-md-2 col-form-label ">{{ __('E-Mail') }}</label>
+            <div class="col-md-8">
+                <div class="card border-dark">
+                    <div class="card-header bg-warning fw-medium border-dark">{{ __('Reset password') }}</div>
 
-                            <div class="col-md-9">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('password.request') }}">
+                            @csrf
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                            <div class="form-group row mb-3 justify-content-center d-flex">
+                                <label for="email" class="col-md-2 col-form-label ">{{ __('E-Mail') }}</label>
+
+                                <div class="col-md-9">
+                                    <input id="email" type="email"
+                                        class="form-control @error('email') is-invalid @enderror" name="email"
+                                        value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+
                             </div>
-            
-                        </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="text-center">
-                                <button type="submit" class="btn btn-dark">
-                                    {{ __('Send Email') }}
-                                </button>
+                            <div class="form-group row mb-0">
+                                <div class="text-center">
+                                    <button type="submit" class="btn btn-dark">
+                                        {{ __('Send Email') }}
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- enologic/resources/views/auth/verify-email.blade.php
+++ enologic/resources/views/auth/verify-email.blade.php
@@ -1,35 +1,40 @@
-
 @extends('auth.template')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        @if(session('status'))
-        <div class="alert alert-success" role="alert">
-            {{session('status')}}
-        </div>
-        @endif
-        <div class="col-md-8">
-            <div class="card border-dark">
-                <div class="card-header bg-warning border-dark fw-medium">{{ __('Email Verification') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            @if (session('status'))  
+            <div class="container d-flex justify-content-center">
+                <div class="col-4 alert alert-info alert-dismissible fade show text-center" role="alert">
+                    <strong>You have been registered successfuly</strong> 
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            </div>
+            @endif
 
-                <div class="card-body">
-                    <p class="fst-italic text-center">You must verify your email address. Please, check your email for a verification link</p>
-                    <form method="POST" action="{{ route('verification.send') }}">
-                        @csrf
 
-                        <div class="form-group row mb-0">
-                            <div class="text-center">
-                                <button type="submit" class="btn btn-dark" value="Resend">
-                                    {{ __('Resend Email') }}
-                                </button>
+            <div class="col-md-8">
+                <div class="card border-dark">
+                    <div class="card-header bg-warning border-dark fw-medium">{{ __('Email Verification') }}</div>
 
+                    <div class="card-body">
+                        <p class="fst-italic text-center">You must verify your email address. Please, check your email for a
+                            verification link</p>
+                        <form method="POST" action="{{ route('verification.send') }}">
+                            @csrf
+
+                            <div class="form-group row mb-0">
+                                <div class="text-center">
+                                    <button type="submit" class="btn btn-dark" value="Resend">
+                                        {{ __('Resend Email') }}
+                                    </button>
+
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
