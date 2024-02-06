b8316591abe1297f4ff6d63d92cbcc665bd53b11
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2024-02-06 12:11:25 +0100

    mod => correccion timeStamps tablas pivotes

--- enologic/app/Models/Cart.php
+++ enologic/app/Models/Cart.php
@@ -21,7 +21,7 @@ class Cart extends Model
 
 
         public function products(){
-            return $this->belongsToMany(Product::class)->withPivot('quantity');
+            return $this->belongsToMany(Product::class)->withPivot('quantity')->withTimestamps();
 
     }
 }
