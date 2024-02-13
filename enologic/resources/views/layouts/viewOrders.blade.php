e8bb997e207cff2117d10580714dbcab7a45a097
David <gdavid1004@alumnos.ilernasevilla.com>
2024-02-13 20:09:46 +0100

    ADD => VIsta Pedidos + Modal

--- enologic/resources/views/layouts/viewOrders.blade.php
+++ enologic/resources/views/layouts/viewOrders.blade.php
@@ -18,11 +18,54 @@
                         <tr>
                             <td>{{ $order->id }}</td>
                             <td>{{ $order->created_at }}</td>
-
+                            <td>
+                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#orderModal{{ $order->id }}">
+                                    View Details
+                                </button>
+                            </td>
                         </tr>
                     @endforeach
                 </tbody>
             </table>
+
+            <!-- Modales fuera del bucle -->
+            @foreach ($orders as $order)
+                <div class="modal fade" id="orderModal{{ $order->id }}" tabindex="-1" aria-labelledby="orderModalLabel{{ $order->id }}" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="orderModalLabel{{ $order->id }}">Order Details - {{ $order->id }}</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>Product Name</th>
+                                            <th>Price</th>
+                                            <th>Quantity</th>
+                                            <th>Total</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($order->products as $product)
+                                            <tr class="align-middle">
+                                                <td>{{ $product->product_name }}</td>
+                                                <td class="text-center">{{ $product->price }} €</td>
+                                                <td class="text-center">{{ $product->pivot->quantity }}</td>
+                                                <td class="text-center">{{ $product->price * $product->pivot->quantity }} €</td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
         @else
             <p>No tienes pedidos.</p>
         @endif
